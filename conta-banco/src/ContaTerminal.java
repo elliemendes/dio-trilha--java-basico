7457208876b4d1d7bc7cf34d447347fa42d8ca6c
ellie <elliesantosm@gmail.com>
2024-06-08 23:37:53 -0300

    Implementação da leitura de dados via terminal e mensagem de boas-vindas

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,39 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            // Solicite e receba a agência
+            System.out.println("Por favor, digite o número da Agência:");
+            String agencia = scanner.nextLine();
+
+            // Solicite e receba a conta
+            System.out.println("Digite o número da Conta:");
+            int numero = scanner.nextInt();
+            scanner.nextLine();  // Limpa o buffer do teclado
+
+            // Solicite e receba o nome do cliente
+            System.out.println("Digite o Nome do Cliente:");
+            String nomeCliente = scanner.nextLine();
+
+            // Solicite e receba o saldo
+            System.out.println("Por favor, digite o saldo:");
+            double saldo = scanner.nextDouble();
+            scanner.nextLine();  // Limpa o buffer do teclado
+
+            // Crie a mensagem final e exibe
+            String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. "
+                    + "Sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+            System.out.println(mensagem);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: Entrada inválida. Por favor, certifique-se de digitar os valores corretamente.");
+        } finally {
+            // Fecha o scanner
+            scanner.close();
+        }
     }
 }
